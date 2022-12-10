75f90976ea1b102178348c76b11363424d4776d7
Salvato Luice <salvatoluice5@gmai.com>
2022-12-10 19:19:05 +0300

    Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import './navbar.css'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div className='app_navbar'>
+        <div className='app_navbar-logo'>
+            <h1>M-Commerce</h1>
+        </div>
+        <div className='app_navbar-links'>
+            <ul>
+                <li><a href=''>Home</a></li>
+                <li><a href=''>About</a></li>
+                <li><a href=''>Products</a></li>
+                <li><a href=''>Contact</a></li>
+            </ul>
+        </div>
+        <div className='app_navbar-login'>
+            <button>Login</button>
+            <button>Sign Up</button>
+            <button>Cart (0) </button>
+        </div>
+    </div>
   )
 }
 
